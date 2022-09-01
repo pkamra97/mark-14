522d0d85ead7954c09427df84edf4ebc392cccb1
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-09-01 12:12:21 +0530

    fix : styling

--- app.js
+++ app.js
@@ -10,18 +10,22 @@ function calculatePNL(initial, current , quantity)
 {
         if(current<initial)
     {//loss report 
-        var loss=(initial-current)*quantity;
-        var losspercent=(loss/initial)*100;
+        var loss=financial((initial-current)*quantity);
+        var losspercent=financial((loss/initial)*100);
+        
         showOutput(`Hey the loss is ${loss} and the percent is ${losspercent}%`);
+        output_box.style.color = 'red';
     }
     else if(current>initial)
     {   //profit report
-        var profit=(current-initial)*quantity;
-        var profitpercent=(profit/initial)*100;
-        showOutput(`Hey the profit is ${profit} and the percent is ${profitpercent}%`);    
+        var profit=financial((current-initial)*quantity);
+        var profitpercent=financial((profit/initial)*100);
+        showOutput(`Hey the profit is ${profit} and the percent is ${profitpercent}%`); 
+        output_box.style.color = 'green';   
     }
     else{
         showOutput(`No pain , no gain `);
+        output_box.style.color = '#6d28d9'; 
     }
 }
 else{ showOutput("Please fill Every box")}
@@ -45,9 +49,10 @@ function showOutput(message , status)
 }
 
 function financial(x) {  return Number.parseFloat(x).toFixed(2);  }
+// this function will round off upto 2 digits
 
-// working separtely but not working inside loss and profit 1st doubt 
-console.log(financial(2.8956322) )
+// console.log(financial(2.8956322) )
+
   
 
 btn.addEventListener('click',submitBtn);
