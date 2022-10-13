8007fee014dc7a1f144b8429209e01b6bc7688f6
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-10-13 16:16:07 +0530

    fix : percentage and styling

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const output_box=document.querySelector("#output-box");
 
 function calculatePNL(initial, current , quantity)
 {
-    if(initial && current && quantity)
+    if(initial >0 && current >0 && quantity >0)
 {
         if(current<initial)
     {//loss report 
         var loss=financial((initial-current)*quantity);
-        var losspercent=financial((loss/initial)*100);
+        // var losspercent=financial((loss/initial)*100);
+
+        var losspercent=financial(100*(loss/(initial*quantity)));
         
         showOutput(`Hey the loss is ${loss} and the percent is ${losspercent}%`);
         output_box.style.color = 'red';
@@ -19,7 +21,7 @@ function calculatePNL(initial, current , quantity)
     else if(current>initial)
     {   //profit report
         var profit=financial((current-initial)*quantity);
-        var profitpercent=financial((profit/initial)*100);
+        var profitpercent=financial((profit/(initial*quantity))*100);
         showOutput(`Hey the profit is ${profit} and the percent is ${profitpercent}%`); 
         output_box.style.color = 'green';   
     }
@@ -28,7 +30,9 @@ function calculatePNL(initial, current , quantity)
         output_box.style.color = '#6d28d9'; 
     }
 }
-else{ showOutput("Please fill Every box")}
+else{ showOutput("Please fill Every box with Positive Numeric values only");
+output_box.style.color="blue";
+}
 
 }
 
@@ -53,7 +57,7 @@ function financial(x) {  return Number.parseFloat(x).toFixed(2);  }
 
 // console.log(financial(2.8956322) )
 
-  
+
 
 btn.addEventListener('click',submitBtn);
 
